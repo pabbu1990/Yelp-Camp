493993bd9891704556f434bcae9cdfec2dd797b8
Kiran Pabbu <kiranpabbu.90@gmail.com>
2017-08-08 18:00:19 +0000

    added cors

--- app.js
+++ app.js
@@ -18,6 +18,13 @@ mongoose.connect("mongodb://kiran:yelpcamp@ds161890.mlab.com:61890/pabbu_yelpcam
 app.set("view engine", "ejs");
 var bodyParser = require("body-parser");
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(methodOverride("_method"));
